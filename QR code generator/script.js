e9896fdd86877d69b18ff1401b3c85a77fc512da
Raheel <60685319+raheel201@users.noreply.github.com>
2023-03-06 10:53:46 +0530

    Add files via upload

---
+++ QR code generator/script.js
@@ -0,0 +1,16 @@
+let imgbox = document.getElementById("imgBox");
+let qrimg = document.getElementById("qrImage");
+let qrtext = document.getElementById("qrText");
+
+
+function generateQR(){
+    if(qrtext.value.length > 0){
+        qrimg.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+        imgbox.classList.add("show-img");
+    }else{
+        qrtext.classList.add('error')
+        setTimeout(()=>{
+            qrtext.classList.remove('error');
+        },1000)
+    }
+}
